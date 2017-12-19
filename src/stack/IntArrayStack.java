b08bc9ab35c14946ee8c8fdcaf178d599611a94f
eyuel <eyueltb@gmail.com>
2017-12-19 13:25:54 +0100

    "stack implementaion using array for Integer data type"

---
+++ src/stack/IntArrayStack.java
@@ -0,0 +1,32 @@
+package stack;
+
+public class IntArrayStack {
+
+	private int values[];
+	private int top = 0;
+
+	public IntArrayStack(int size) {
+
+		values = new int[size];
+	}
+
+	public void push(int num) {
+		values[top++] = num;
+	}
+
+	public int pop() {
+		return values[--top];
+	}
+
+	public int top() {
+		return values[top - 1]; // assign,decrement top then return
+	}
+
+	public int size() {
+		return top;
+	}
+
+	public boolean isEmpty() {
+		return top == 0;
+	}
+}
